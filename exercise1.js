a9fa4b82e830630e8b52f34443869ee395f1ef62
Caroline Avery Tanto <75901018+carolinetanto@users.noreply.github.com>
2023-09-05 23:53:20 +0800

    test

---
+++ exercise1.js
@@ -0,0 +1,12 @@
+let x = 5;
+const y = 3;
+
+console.log(x);
+console.log(y);
+
+let buah = ["apel", "jeruk", "pisang", "stroberi", "nanas"];
+console.log(buah);
+buah.push("semangka");
+console.log(buah);
+buah.pop();
+console.log(buah);
